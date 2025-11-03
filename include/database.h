8367323d81092046ac692b56d92d0e9590db440d
ishu-developer <ishudeveloper@zohomail.in>
2025-11-03 18:37:53 +0530

    Update project structure and remove binary files

--- include/database.h
+++ include/database.h
@@ -3,9 +3,9 @@
 
 // Database related declarations
 
-int db_init(void);
-int db_create_tables(void);
-void db_close(void);
+int db_init(void);                    // Returns int (0 for success, -1 for error)
+int db_create_tables(void);           // Returns int
+void db_close(void);                  // Returns void
 
 void connect_database(void);
 void disconnect_database(void);
--- include/payroll.h
+++
@@ -1,6 +0,0 @@
-#ifndef PAYROLL_H
-#define PAYROLL_H
-
-void process_payroll(void);
-
-#endif // PAYROLL_H
